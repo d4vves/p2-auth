80c37c1ed01386ecee5c14584906f0e4b4f907b8
d4vves <d4vves@gmail.com>
2020-06-29 17:52:27 -0500

    added post route for user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const flash = require('flash')
 
 router.get('/register', (req, res) => {
     res.render('auth/register')
 })
 
-// router.post('/register', (req, res) => {
-
-// })
+router.post('/register', (req, res) => {
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        },
+        defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    }).then(([user, created]) => {
+        if (created) {
+            // auth
+            console.log(`🤘 User created. 🤘`)
+            res.redirect('/')
+        } else {
+            console.log(`👎 User email exists. 👎`)
+            req.flash('error', 'Email already exists. Please try again.')
+            res.redirect('/auth/register')
+        }
+    }).catch((err) => {
+        console.log(`🚦 ${err.message}\n${err} 🚦`)
+        req.flash('error', err.message)
+        res.redirect('/auth/register')
+    })
+})
 
 router.get('/login', (req, res) => {
     res.render('auth/login')
@@ -17,3 +40,5 @@ router.get('/login', (req, res) => {
 // router.post('/login', (req, res) => {
 
 // })
+
+module.exports = router
