c314101aef3d8b1b0ee78c6633c62f90cc89860a
d4vves <d4vves@gmail.com>
2020-06-30 17:47:07 -0500

    added navbar to layout.ejs page

--- controllers/auth.js
+++ controllers/auth.js
@@ -66,4 +66,9 @@ router.post('/login', passport.authenticate('local', {
     failureFlash: 'Invalid username or password.'
 }))
 
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 module.exports = router
